d1888b184f3450f0a2069c2ebfac1f9f3e27bf3b
Anton Trv <abtsochi@gmail.com>
2020-02-07 11:55:39 +0300

    Updates

---
+++ precache-manifest.09b8e9a5e95dd1472ad1f82b6f2c1481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f89806586dad42946add1cbe92f2c67",
+    "url": "/unimeta/index.html"
+  },
+  {
+    "revision": "b64e0634525d6d0cf00e",
+    "url": "/unimeta/static/css/main.1ce73382.chunk.css"
+  },
+  {
+    "revision": "665df16d54a9833da5f0",
+    "url": "/unimeta/static/js/2.3fb07c7e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/unimeta/static/js/2.3fb07c7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b64e0634525d6d0cf00e",
+    "url": "/unimeta/static/js/main.10e5409d.chunk.js"
+  },
+  {
+    "revision": "fd86a385c70826dcf0dd",
+    "url": "/unimeta/static/js/runtime-main.f0c1bd18.js"
+  }
+]);
