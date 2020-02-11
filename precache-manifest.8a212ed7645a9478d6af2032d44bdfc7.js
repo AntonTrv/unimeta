8d04fa8aaf43e79ab3dad9c7d9717085987bb1f7
Anton Trv <abtsochi@gmail.com>
2020-02-11 10:39:40 +0300

    Updates

---
+++ precache-manifest.8a212ed7645a9478d6af2032d44bdfc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc7be34a9ae3d57814058f36a6cd8ef",
+    "url": "/unimeta/index.html"
+  },
+  {
+    "revision": "4ee38dcda95dbd81e525",
+    "url": "/unimeta/static/css/main.7571bc01.chunk.css"
+  },
+  {
+    "revision": "6ea819cf46f8399da971",
+    "url": "/unimeta/static/js/2.2d81ba21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/unimeta/static/js/2.2d81ba21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ee38dcda95dbd81e525",
+    "url": "/unimeta/static/js/main.ece8ec78.chunk.js"
+  },
+  {
+    "revision": "fd86a385c70826dcf0dd",
+    "url": "/unimeta/static/js/runtime-main.f0c1bd18.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/unimeta/precache-manifest.09b8e9a5e95dd1472ad1f82b6f2c1481.js"
+  "/unimeta/precache-manifest.8a212ed7645a9478d6af2032d44bdfc7.js"
 );
 
 self.addEventListener('message', (event) => {
